f27cac168554b7d0397b33d1f586e92259b9a9c8
Alberto Lazari <alberto.lazari@gmail.com>
2023-03-16 17:15:48 +0100

    Test script

---
+++ run.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+file=polyglot.c
+# C
+gcc $file && {
+    ./a.out
+    rm a.out
+}
+
+# C++
+g++ -x c++ $file && {
+    ./a.out
+    rm a.out
+}
+
+# Php
+php $file
+
+# Python
+python3 $file
+
+# Shell
+sh $file
