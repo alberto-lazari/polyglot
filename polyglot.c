fba627c8356daff92d997059ae0a9a3409bbb350
Alberto Lazari <alberto.lazari@gmail.com>
2023-03-16 17:15:28 +0100

    Polyglot script added

---
+++ polyglot.c
@@ -0,0 +1,35 @@
+#define C
+#ifndef C
+""" " &> /dev/null
+# Shell code
+echo Shell
+exit 0
+<?php
+for ($i = 0; $i < 6; ++$i) {
+    echo "                   ";
+    echo chr(27) . "[A";
+    for ($j = 0; $j < 19; ++$j) {
+        echo "\x08";
+    }
+}
+echo "                   ";
+for ($j = 0; $j < 19; ++$j) {
+    echo "\x08";
+}
+// Php code
+echo "php\n";
+exit();
+?>
+"\
+"""
+# Python code
+print('Python')
+#endif
+#ifndef __cplusplus
+#include <stdio.h>
+#define exit() /* C code */ int main() { printf("C\n"); }
+#else
+#include <iostream>
+#define exit() /* C++ code */ int main() { std::cout << "C++\n"; }
+#endif
+exit()
